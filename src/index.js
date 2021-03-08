d204c2ba3621e713afa22849d3250e48d758720e
Andy Mopad <andy.mopad@gmail.com>
2021-03-08 18:19:23 +0000

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+class Todo {
+    constructor (name, desc, priority, duedate){
+        this.name = name;
+        this.desc = desc;
+        this.priority = priority;
+        this.duedate = duedate;
+    }
+    todoDisplay() {
+        return 
+    }
+}
